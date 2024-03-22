fee8577404c9a0881b9ee515f9cfeb47132f29a0
prAch12020 <prachi.p@yahoo.com>
2024-03-22 18:05:50 +0300

    Final Commit

--- src/test/java/com/prachi/un_broke/UnBrokeApplicationTests.java
+++ src/test/java/com/prachi/un_broke/UnBrokeApplicationTests.java
@@ -1,28 +1,11 @@
 package com.prachi.un_broke;
 
-import com.prachi.un_broke.model.Category;
-import com.prachi.un_broke.service.CategoryService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.hamcrest.Matchers.hasSize;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
 
 @ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
